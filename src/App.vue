9e383e3191b2118f60ac797ac0de4cfce4a50dc6
x_x <x_x>
2018-04-28 11:42:29 -0400

    updated/redesigned portfolio website
    updated/redesigned portfolio website

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,11 @@
 <template>
   <div id="app">
-    <router-view> </router-view>
+    <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  computed: {
-    background () {
-      return require('@/assets/images/black.jpg')
-    }
-  }
+  name: 'app'
 }
 </script>
-
-<style>
-#app {
-  /* background-color: #2980b9; */
-  display:table;
-  align-items:center;
-  justify-content:center;
-  font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  width: 100vw;
-  height: 100vh;
-}
-</style>
---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createLogger from 'vuex/dist/logger'
+import createPersistedState from 'vuex-persistedstate'
+import user from '@/store/modules/user'
+
+Vue.use(Vuex)
+const debug = process.env.NODE_ENV !== 'production'
+export default new Vuex.Store({
+  modules: {
+    user
+  },
+  strict: debug,
+  plugins: debug ? [createLogger(), createPersistedState()] : [createPersistedState()]
+})
